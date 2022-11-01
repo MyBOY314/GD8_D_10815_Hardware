90400522709ea9d2ab3e89f9c3aacc68415c2d01
Dave <davehabelpaprindey@gmail.com>
2022-11-01 08:16:14 +0700

    GD8_CAMERA_D_10815

---
+++ app/src/main/java/com/example/gd8_accelerometer_d_10815/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gd8_accelerometer_d_10815
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gd8_camera_d_10815.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
